1b6070fdf7595b0fb80a230f87726a9ab4010df6
220508 <te220508@gmail.com>
2025-06-03 00:37:55 +0900

    kadai12

--- src/main/java/jp/te4a/spring/boot/myapp12/mybootapp12/Writter.java
+++ src/main/java/jp/te4a/spring/boot/myapp12/mybootapp12/Writter.java
@@ -1,4 +1,4 @@
-package jp.te4a.spring.boot.myapp11.mybootapp11;
+package jp.te4a.spring.boot.myapp12.mybootapp12;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -10,17 +10,17 @@ import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
 @Documented
-@Constraint(validatedBy = TestValidator.class)
+@Constraint(validatedBy = WritterValidator.class)
 @Target({ElementType.METHOD,ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 
-public @interface TestValid {
-    String param();
-    String message() default "input other than {param}.";
+public @interface Writter {
+    String ok();
+    String message() default "input {ok}.";
     // Classオブジェクトを得る（戻り値とする）メソッドgroups()
     // デフォルト値は空のクラス
     Class<?>[] groups() default {};
     // Payloadクラスを継承したClassオブジェクトを得る
     // （戻り値とする）メソッドpayload()、デフォルト値は空のクラス
     Class<? extends Payload>[] payload() default{};
-    }
+}
