d707f20d47e0155f1296374672678681c5b46cf9
220508 <te220508@gmail.com>
2025-06-03 08:53:51 +0900

    kadai13

---
+++ src/main/java/jp/te4a/spring/boot/myapp13/service/LoginUserDetailsService.java
@@ -0,0 +1,33 @@
+package jp.te4a.spring.boot.myapp13.service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import jp.te4a.spring.boot.myapp13.bean.UserBean;
+import jp.te4a.spring.boot.myapp13.repository.UserRepository;
+import jp.te4a.spring.boot.myapp13.security.LoginUserDetails;
+
+@Service
+public class LoginUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserBean> opt = userRepository.findById(username);
+        UserBean userBean = opt.orElseThrow(() -> new UsernameNotFoundException("The requested user is not found."));
+        return new LoginUserDetails(userBean, true, true, true, getAuthorities(userBean));
+    }
+    private Collection<GrantedAuthority> getAuthorities(UserBean userBean) {
+        return AuthorityUtils.createAuthorityList("ROLE_USER");
+    }
+}
+
+    
