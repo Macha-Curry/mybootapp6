1b865dfb8da1a8447cd0edbb38b691493a42a1fb
220508 <te220508@gmail.com>
2025-06-04 16:29:41 +0900

    kadai10 seccond commit

--- src/main/java/jp/te4a/spring/boot/myapp10/mybootapp10/BookRepository.java
+++ src/main/java/jp/te4a/spring/boot/myapp10/mybootapp10/BookRepository.java
@@ -1,11 +1,6 @@
 package jp.te4a.spring.boot.myapp10.mybootapp10;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
-import org.springframework.beans.BeanUtils;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
